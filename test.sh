eb5e31d324646809577f04d77f668e01d4891ca4
refoldingcrystal <refoldingcrystal@proton.me>
2025-12-28 19:49:37 +0100

    naprawione blad zwiazany z CR w testach

--- test.sh
+++ test.sh
@@ -11,7 +11,7 @@ for infile in test/in*; do
     fi
 
     echo -n "$filename : "
-    if diff -q <(./main < "$infile") "$outfile" > /dev/null; then
+    if diff -qw <(./main < "$infile") "$outfile" > /dev/null; then
         echo "PASS"
     else
         echo "FAIL"
